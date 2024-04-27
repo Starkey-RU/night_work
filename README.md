e0adf7ad618b70ea65a549280aa52a44226de73a
Starkey-RU <muhachev.firs@gmail.com>
2024-04-27 12:09:13 +0300

    "note" completion, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Фирс.
+
+Education purposes only.
--- src/main/java/night_work/Note.java
+++ src/main/java/night_work/Note.java
@@ -1,3 +1,54 @@
 package night_work;
+import jakarta.persistence.*;
+
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "folder_id")
+    private Folder folder;
+
+    public Note() {}
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
